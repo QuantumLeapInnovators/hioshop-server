73f24f62ca4ceaea6ee6fa4cef3a84b8a81e9be4
v2 <v2>
2023-11-22 14:32:16 +0800

    fix update user by wx

--- src/api/controller/auth.js
+++ src/api/controller/auth.js
@@ -8,8 +8,9 @@ module.exports = class extends Base {
     const code = this.post("code");
     const username = this.post("username");
     const password = this.post("password");
+    const wxUserInfo = this.post("wxUserInfo");
     let currentTime = parseInt(new Date().getTime() / 1000);
-    const clientIp = ""; // 暂时不记录 ip test git
+    const clientIp = this.ctx.ip;
 
     // params
 
@@ -26,6 +27,7 @@ module.exports = class extends Base {
     };
     let sessionData = await rp(options);
     sessionData = JSON.parse(sessionData);
+    console.info("sessionData", sessionData)
     if (!sessionData.openid) {
       return this.fail("登录失败，openid无效");
     }
@@ -39,7 +41,7 @@ module.exports = class extends Base {
     let userInfo = await this.model("user").where({username: username}).find();
 
     let is_new = 0;
-    const buffer = Buffer.from('微信用户');
+    // const buffer = Buffer.from('微信用户');
     let nickname = '微信用户';
     if (think.isEmpty(userInfo)) {
 
@@ -72,14 +74,36 @@ module.exports = class extends Base {
 
     sessionData.user_id = userInfo.id;
     // 更新登录信息
+    let updateInfo = {
+      last_login_time: currentTime,
+      last_login_ip: clientIp,
+      weixin_openid: sessionData.openid,
+    }
+    if(wxUserInfo){
+      // {
+      //   avatarUrl: ""
+      //   city: ""
+      //   country: ""
+      //   gender: 0
+      //   language: "zh_CN"
+      //   nickName: "Fool"
+      //   province: ""
+      // }
+      if(wxUserInfo.nickName && userInfo.nickname != wxUserInfo.nickName){
+        updateInfo.nickname = wxUserInfo.nickName
+      }
+      if(wxUserInfo.avatarUrl && userInfo.avatar != wxUserInfo.avatarUrl){
+        updateInfo.avatar = wxUserInfo.avatarUrl
+      }
+      if(wxUserInfo.gender && userInfo.gender != wxUserInfo.gender){
+        updateInfo.gender = wxUserInfo.gender
+      }
+    }
     await this.model("user")
       .where({
         id: userInfo.id,
       })
-      .update({
-        last_login_time: currentTime,
-        last_login_ip: clientIp,
-      });
+      .update(updateInfo);
     const newUserInfo = await this.model("user")
       .field("id,username,nickname, avatar, is_ban")
       .where({
